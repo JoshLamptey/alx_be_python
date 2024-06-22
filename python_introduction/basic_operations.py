a300be99617880a6e9a689261e5a7c4b459424a9
JoshLamptey <joshlamptey75@gmail.com>
2024-06-22 07:55:53 -1200

    a script running basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#the variables for number1 and number2
+number1 = 10
+number2 =5
+
+#perform the addition,subtraction and multiplication on these numbers
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+#print the results of the operation in a readable formart
+print("the sum of ",number1 ,"and ",number2 ,'is' ,sum)
+print("the product of ",number1, "and",number2, "is ", product)
+print("the difference between",number1, "and",number2, "is",difference)
