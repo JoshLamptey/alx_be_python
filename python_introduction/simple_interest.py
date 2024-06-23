f68ea7189c5d3dd1971a67b590a08acca8ac9aee
JoshLamptey <joshlamptey75@gmail.com>
2024-06-22 19:11:45 -1200

    corrections to the script

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+principal = 1000 #the startup amount (principal investment)
+rate = 0.05 
+time = 3 #the time frame for the investment
+
+#calculate the simple interest using the provided formula
+interest = principal * rate * time
+
+print("the simple interest is :", interest)
