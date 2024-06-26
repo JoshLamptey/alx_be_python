c418a59ee4ff2f7c00b44044f449a4dd42da5cbd
JoshLamptey <joshlamptey75@gmail.com>
2024-06-25 22:17:35 -1200

    a new folder for this weeks project and the first project

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -4,9 +4,9 @@ input("What's the weather like today ? (sunny/rainy/cold):")
 # use the input to give the recommendations
 if input == "sunny":
     print("Wear a t-shirt and sunglasses."),
-    elif input =="rainy":
-        print("Don't forget your umbrella and a raincoat.")
-    elif input == "cold":
-        print("Make sure to wear a warm coat and a scarf.")
-    else:
-        print("Sorry, i don't have the recommendations for this weather.")   
+elif input =="rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif input == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, i don't have the recommendations for this weather.")   
