b5950e56e84794c9f99f203367de9617d7d43393
JoshLamptey <joshlamptey75@gmail.com>
2024-06-28 22:14:09 -1200

    a daily reminder script

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,27 +1,27 @@
 #prompt the user to give you a task description
-task = input("What is the task please?")
+Task = input("What is the task please?")
 # prompt the user for the priority level of the task
-priority = input("What is the priority level of this task please?")
+Priority = input("What is the priority level of this task please?")
 #ask if its time bound or not
-time_bound = input("is it time bound? Please respond with yes/no")
+Time_Bound = input("is it time bound? Please respond with yes/no")
 
 #check the answers
-if priority not in ["high","medium","low"]:
+if Priority not in ["high","medium","low"]:
     print("Invalid priority level. Please choose between 'high', 'medium', 'low'")
-elif time_bound not in ['yes','no']:
+elif Time_Bound not in ['yes','no']:
     print("Invalid response.Please enter 'yes','no'")
 else:
     #process the task based on the priority and time sensitivity
-    match priority:
+    match Priority:
         case 'high':
-            reminder = f"Reminder: '{task}' is a high priority task that requires immediate attention!"
+            reminder = f"Reminder: '{Task}' is a high priority task that requires immediate attention!"
         case 'medium':
-            reminder = f"Reminder: '{task}' is a medium priority task. Try to complete it soon."
+            reminder = f"Reminder: '{Task}' is a medium priority task. Try to complete it soon."
         case 'low':
-            reminder = f"Reminder: '{task}' is a low priority task. Consider completing it when you have free time."
+            reminder = f"Reminder: '{Task}' is a low priority task. Consider completing it when you have free time."
         
     #edit the reminder if the task is time-bound
-    if time_bound == 'yes':
+    if Time_Bound == 'yes':
         reminder += " Don't forget, it needs to be done today!"
 
         #print the reminder
