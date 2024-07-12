694442f2dc700418eeb4eea5b09582ddd1172e5e
JoshLamptey <joshlamptey75@gmail.com>
2024-07-12 10:20:08 -1200

    finished the script for the bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+#initialise the class
+class BankAccount:
+    def __init__(self,initial_balance = 0.0):
+        self.current_balance = initial_balance
+
+
+    def deposit(self,amount):
+        self.current_balance = amount + self.current_balance
+        return self.current_balance
+
+    def withdraw(self,amount):
+        if self.current_balance >= amount:
+            self.current_balance =  amount - self.current_balance
+            return True
+        else:
+            return False
+        
+
+    def display_balance(self):
+        return print(f"${self.current_balance:.2f} is your current balance")
