d31f9d71c908f4ddcc49b73d86cf60e53d297574
JoshLamptey <joshlamptey75@gmail.com>
2024-07-13 02:22:52 -1200

    finished working on robust division

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -8,9 +8,8 @@ def safe_divide(numerator,denominator):
 
         result = num / den
         return f"The result of the division is {result:.1f}"
-
-    except ZeroDivisionError:
-        return "Error: Cannot be divide by zero."
     
+    except ZeroDivisionError:
+        return "Error: Cannot divide by zero."
     except ValueError:
         return "Error: Please enter numeric values only."
